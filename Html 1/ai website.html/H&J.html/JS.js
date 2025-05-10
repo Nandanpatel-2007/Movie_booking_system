0d69cfdbb7b7ee6a94c367538b3ad0a0ffa6d84c
Nandanpatel-2007 <nandanpatelbr@gmail.com>
2025-05-10 14:18:00 +0530

    commit changes

---
+++ Html 1/ai website.html/H&J.html/JS.js
@@ -0,0 +1,8 @@
+let NP = document.getElementById("heading")
+NP.innerHTML="Name: Nandan patel"
+let NM = document.getElementById("hi")
+NM.innerHTML="Age:18"
+let NY= document.getElementById("bye")
+NY.innerHTML="Class:CSE 1"
+let NO = document.getElementById("jai")
+NO.innerHTML="college:REVA"
---
+++ Html 1/hi.html/J.js
@@ -0,0 +1,18 @@
+function data(){
+ let a = document.getElementById("firstname")
+ let data = a.value
+ let display = document.getElementById("display")
+ display.innerHTML = data
+ 
+ let b = document.getElementById("ID")
+ let dat = b.value
+ let displa = document.getElementById("displa")
+ displa.innerHTML = dat
+
+ let c = document.getElementById("college")
+ let da = c.value
+ let displ = document.getElementById("displ")
+ displ.innerHTML = da
+}
+
+
